8b1c8e68bc0045f6299b61a31f522fe0f5c49f27
Priyanshu Kardam <priyanshukardam223@gmail.com>
2026-07-31 14:21:01 +0530

    Time: 3 ms (7.53%) | Memory: 43.6 MB (11.10%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -7,23 +7,7 @@ class Solution {
         int i=0;
         int k=0;
              if(n!=m) return false;
-        //     char st=ss.charAt(i);
-        //     char end=goal.charAt(k);
-       
-        // while(st!=end && i<nn && k<m ){
-        //      st=ss.charAt(i);
-        //      end=goal.charAt(k);
-            
-        //         i++;
-            
-        // }
-        // if(i>=nn || j>=nn ) return false;
-        // while(i<nn && k<m){
-        //        st=ss.charAt(i++);
-            
-        //      end=goal.charAt(k++);
-        //     if(st!=end) return false;
-        // }
+  
        return (s + s).contains(goal);
     }
 }
