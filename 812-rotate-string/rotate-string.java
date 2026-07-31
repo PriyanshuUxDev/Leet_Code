6550f7f61bd0fb2164f4a810617d206024ef2f21
Priyanshu Kardam <priyanshukardam223@gmail.com>
2026-07-31 14:22:33 +0530

    Simplify rotateString method implementation

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -2,12 +2,9 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         int n=s.length();
         String ss=s+s;
-        int nn=ss.length();
         int m=goal.length();
-        int i=0;
-        int k=0;
              if(n!=m) return false;
   
-       return (s + s).contains(goal);
+       return (ss).contains(goal);
     }
 }
